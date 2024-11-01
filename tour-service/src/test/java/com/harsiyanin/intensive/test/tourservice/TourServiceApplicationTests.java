e5db6e4e7349eca7e8c51ccb396306be8eacaf3a
Timirlan Kharsiev <harsiyanin@gmail.com>
2024-11-01 21:05:29 +0300

    add ticket-service

---
+++ tour-service/src/test/java/com/harsiyanin/intensive/test/tourservice/TourServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsiyanin.intensive.test.tourservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
